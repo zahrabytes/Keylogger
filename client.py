8c3afca0fa91739d83e91d18a4fa183be63452b6
Zahra Bukhari <146145027+zahrabytes@users.noreply.github.com>
2024-01-04 18:43:22 -0600

    fixed mistake

--- client.py
+++ client.py
@@ -3,6 +3,6 @@ import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((socket.gethostname(), 6060))
 
-message = s.resv(2048)
+message = s.recv(2048)
 
 print(f"Message received: {message}")
