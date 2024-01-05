68b4593c719ef2d3dd2f9594d9a7eb4b79ca7c80
Zahra Bukhari <146145027+zahrabytes@users.noreply.github.com>
2024-01-04 18:05:48 -0600

    server complete

--- server.py
+++ server.py
@@ -1,1 +1,12 @@
 
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((socket.gethostname(), 6060))
+s.listen(5)
+
+while True: 
+    clientSocket, address = s.accept()
+    print(f"Connection established from address: {address}")
+    clientSocket.send(bytes("Welcome to the server", "utf-8"))
+    clientSocket.close()
